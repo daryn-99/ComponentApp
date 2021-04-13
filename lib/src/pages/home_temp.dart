540965b2f50514bfb87acc92f7214de876e78c39
daryn-99 <74755232+daryn-99@users.noreply.github.com>
2021-04-12 20:06:02 -0600

    Add files via upload

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,35 @@
+/*import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes temp'),
+      ),
+      body: ListView(
+          //children: _crearItems()
+          children: _crearItemsCorta()),
+    );
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Sub titulo'),
+            leading: Icon(Icons.attribution_outlined),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
+}
+*/
